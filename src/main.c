de94d0f75793797035ba139f685d8d7128eeeff0
wuli133144 <1683358846@qq.com>
2017-08-11 17:45:26 +0800

    man.c

--- src/main.c
+++ src/main.c
@@ -4,24 +4,35 @@
  test demo
 */
 
-#include "../inc/utils.h"
+//#include "../inc/utils.h"
+
+#include"../inc/mem_pool.h"
 
 
 int main(int argc,char *const argv[])
 {    
 
    
-     void *alloc;
+     char *alloc;
 
     // printf("%d ",mem_align(p));
 
-      
-      struct mem_pool_node *p=NULL;
-       
+      mem_pool_t *p=NULL;     
       p=jw_mem_alloc(1240);
-      //printf("%p=========",p);
-      //  alloc=jw_mem_palloc(p,24);
-      //  printf("%p=========",alloc);
+
+      printf("=====pool addr====%p\n",p);
+      printf("======pool->end===%p\n",p->end);
+      printf("real size=%d\n",p->end-(char*)p);
+      printf("======pool->last===%p\n",p->last);
+      printf("last size %d\n",p->last-(char *)p);
+      printf("======pool->next===%p\n",p->next);
+      
+
+
+       alloc=jw_mem_palloc(p,24);
+       printf("%d=========",alloc-(char *)p);
+
+
 
 
 
